0346e9c3c01c17fae25fb7f9e83c2984cc8c32ea
Mubeen-creator <142151394+Mubeen-creator@users.noreply.github.com>
2024-04-17 20:12:04 -0700

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        SPACE_ID: "qro7jddzebmo",
+        CONTENTFUL_ACCESS_KEY: "nsOBMvQsrxAUw5il7ifI166lx_zg5tvomHTz8CgF3Qw"
+      },
+      images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'images.ctfassets.net',
+            port: '',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
